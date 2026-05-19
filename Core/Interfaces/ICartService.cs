6ffe86fbc2f364ff6a0e3c883bf29e8e8a758d66
Carlos Rene Ponce Novelo <crpn81@yahoo.com.mx>
2026-05-19 14:54:09 -0600

    Cart Service and Redis finished

---
+++ Core/Interfaces/ICartService.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface ICartService
+    {
+
+        Task<ShoppingCart?> GetShoppingCartAsync(string key);
+
+        Task<ShoppingCart?> SetShoppingCartAsync(ShoppingCart cart);
+
+        Task<bool> DeleteCartAsync(string key);
+
+    }
+}
